9805a361df801f081aa7d3dbd703b38582b8edcb
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-03-20 08:06:51 +0100

    EPL-endboss, chicken animation bugfix. implement offset for endboss

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -15,6 +15,7 @@ class Chicken extends MovableObject {
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_DEAD);
 
         this.x = 200 + Math.random() * 500;
         this.speed = 0.15 + Math.random() * 0.25;
