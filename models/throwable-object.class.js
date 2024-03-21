68dee8dc13de7a91b7504bacdfc7d90eefca21c9
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-03-21 06:46:41 +0100

    EPL-throw back

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,18 +1,21 @@
 class ThrowableObject extends MovableObject {
   
-  constructor(x, y) {
+  constructor(x, y, direction) {
     super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
     this.x = x;
     this.y = y;
     this.height = 120;
     this.width = 110;
+    this.otherDirection = direction;
     this.throw();
   }
 
-  throw(x, y) {
+  throw() {
+
+
     this.speedY = 30;
     this.applyGravity();
-    if (this.character.otherDirection == true) { // Korrekte Syntax für die Bedingung
+    if (this.otherDirection == true) { 
       setInterval(() => {
         this.x -= 10;
       }, 25);
