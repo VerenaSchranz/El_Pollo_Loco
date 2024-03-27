7abcc921b87a50777de3946099fcbfa3dbab07da
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-03-27 05:36:59 +0100

    adding restart btn

--- js/game.js
+++ js/game.js
@@ -1,14 +1,20 @@
 let canvas;
 let world;
 let keyboard = new Keyboard();
+let intervalIds = [];
+let muteSoundsInGame = false;
+
 function init() {
   startScreenClose();
   canvas = document.getElementById('canvas');
   world = new World(canvas, keyboard);
 }
 
+
 function startScreenClose() {
   document.getElementById('startScreen').classList.add('d-none');
+  document.getElementById('gameOverScreen').classList.add('d-none');
+
   document.getElementById('canvas').classList.remove('d-none');
 }
 
@@ -63,3 +69,40 @@ document.addEventListener("keyup", (e) => {
     keyboard.D = false;
   }
 });
+
+function gameOver() {
+  document.getElementById("gameOverScreen").classList.remove('d-none');
+  gameisOver();
+}
+
+function winGame() {
+  // document.getElementById("winGameScreen").remove.classList('d-none');
+  document.getElementById("canvas").add.classList('d-none');
+  document.getElementById("winGameScreen").remove.classList('d-none');
+  gameisOver();
+}
+
+function gameisOver() {
+  for (let i = 1; i < 9999; i++){ 
+    window.clearInterval(i);
+  }
+}
+
+function restartGame() {
+  world = new World(canvas, keyboard);
+  document.getElementById("gameOverScreen").classList.add('d-none');
+  document.getElementById("winGameScreen").classList.add('d-none');
+  document.getElementById("canvas").classList.remove('d-none');
+  startScreenClose();
+  stopGame();
+  init();
+}
+
+function stopGame() {
+intervalIds = [];}
+
+
+function setStoppableInterval(fn, time) {
+  let id = setInterval(fn, time);
+  intervalIds.push(id);
+}
