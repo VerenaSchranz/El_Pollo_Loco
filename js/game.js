7113f06138422db43751227773520fe8496bdde5
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-03-28 05:52:33 +0100

    EPL adding popup

--- js/game.js
+++ js/game.js
@@ -37,57 +37,7 @@ function backToMenu() {
   document.getElementById("mute").classList.remove('d-none');
 }
 
-document.addEventListener("keydown", (e) => {
-  if (e.keyCode == 39) {
-    keyboard.RIGHT = true;
-  }
 
-  if (e.keyCode == 37) {
-    keyboard.LEFT = true;
-  }
-
-  if (e.keyCode == 38) {
-    keyboard.UP = true;
-  }
-
-  if (e.keyCode == 40) {
-    keyboard.DOWN = true;
-  }
-
-  if (e.keyCode == 32) {
-    keyboard.SPACE = true;
-  }
-
-  if (e.keyCode == 68) {
-    keyboard.D = true;
-  }
-});
-
-document.addEventListener("keyup", (e) => {
-  if (e.keyCode == 39) {
-    keyboard.RIGHT = false;
-  }
-
-  if (e.keyCode == 37) {
-    keyboard.LEFT = false;
-  }
-
-  if (e.keyCode == 38) {
-    keyboard.UP = false;
-  }
-
-  if (e.keyCode == 40) {
-    keyboard.DOWN = false;
-  }
-
-  if (e.keyCode == 32) {
-    keyboard.SPACE = false;
-  }
-
-  if (e.keyCode == 68) {
-    keyboard.D = false;
-  }
-});
 
 function gameOver() {
   document.getElementById("gameOverScreen").classList.remove('d-none');
@@ -148,3 +98,4 @@ function updateMuteIcon() {
     muteIcon.src = './img/12_icons/sound_off.svg';
   }
 }
+
