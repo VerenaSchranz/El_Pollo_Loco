b78bd350c589a0d39c3b9548cb003833a4ae82cf
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-04-03 06:05:08 +0200

    Update small-chicken.class.js

--- models/small-chicken.class.js
+++ models/small-chicken.class.js
@@ -1,6 +1,6 @@
 class Smallchicken extends MovableObject {
   speed = 5;
-  speedY = 4; 
+  speedY = 4;
   y = 360;
   height = 60;
   width = 60;
@@ -12,15 +12,22 @@ class Smallchicken extends MovableObject {
     bottom: 10,
   }
   IMAGES_WALKING = [
-      'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
-      'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
-      'img/3_enemies_chicken/chicken_small/1_walk/3_w.png',
+    'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+    'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+    'img/3_enemies_chicken/chicken_small/1_walk/3_w.png',
   ];
 
   IMAGES_DEAD = [
     'img/3_enemies_chicken/chicken_small/2_dead/dead.png',
   ];
 
+
+  /**
+   * Constructor for initializing the chicken enemy.
+   *
+   * @param {type} paramName - description of parameter
+   * @return {type} description of return value
+   */
   constructor() {
     super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
     this.loadImages(this.IMAGES_WALKING);
@@ -30,11 +37,21 @@ class Smallchicken extends MovableObject {
     this.animate();
     this.randomizePosition();
   }
-    
+
+  
+  /**
+   * Randomizes the position by setting the x-coordinate to a value between 300 and 2800.
+   */
+
   randomizePosition() {
     this.x = 300 + Math.random() * 2500;
   }
-  
+
+
+  /**
+   * A function to handle the jumping behavior of the chicken character.
+   *
+   */
   chickenJump() {
     if (this.y <= 250) {
       this.speedY = Math.abs(this.speedY);
@@ -45,17 +62,19 @@ class Smallchicken extends MovableObject {
     this.y += this.speedY;
   }
 
+
+  /**
+   * Function for animating the movement and status of the chicken character.
+   */
   animate() {
-    // Animation für das Bewegen nach links
     setInterval(() => {
       this.moveLeft();
     }, 1000 / 60);
 
     setInterval(() => {
       this.chickenJump();
-    }, 1000 / 60); // Ändern Sie die Zeitintervalle nach Bedarf für die gewünschte Animation
+    }, 1000 / 60);
 
-    // Animation basierend auf dem Status des Huhns
     setInterval(() => {
       if (this.isDead === false) {
         this.playAnimation(this.IMAGES_WALKING);
