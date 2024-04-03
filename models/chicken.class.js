21d5f99b893c50797d52d425336cad9f95c36449
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-04-03 05:49:01 +0200

    Update chicken.class.js

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -10,36 +10,51 @@ class Chicken extends MovableObject {
     bottom: 10,
   };
   IMAGES_WALKING = [
-      'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
-      'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
-      'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
+    'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+    'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+    'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
   ];
 
   IMAGES_DEAD = [
     'img/3_enemies_chicken/chicken_normal/2_dead/dead.png',
-];
-  constructor() {
-      super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
-      this.loadImages(this.IMAGES_WALKING);
-      this.loadImages(this.IMAGES_DEAD);
+  ];
 
-      this.x = 200 + Math.random() * 500;
-      this.speed = 0.15 + Math.random() * 0.25;
-      this.animate();
-      this.randomizePosition();
-    }
-    
-    randomizePosition() {
-      this.x = 300 + Math.random() * 2500;
-    }
   
+  /**
+   * Constructor for initializing the object and loading images.
+   */
+  constructor() {
+    super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+    this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_DEAD);
+
+    this.x = 200 + Math.random() * 500;
+    this.speed = 0.15 + Math.random() * 0.25;
+    this.animate();
+    this.randomizePosition();
+  }
+
+
+  /**
+   * A description of the entire function.
+   *
+   */
+  randomizePosition() {
+    this.x = 300 + Math.random() * 2500;
+  }
+
+
+  /**
+   * Animate the object by moving it left and playing different animations based on its state.
+   *
+   * @param None
+   * @return None
+   */
   animate() {
-    // Animation für das Bewegen nach links
     setInterval(() => {
       this.moveLeft();
     }, 1000 / 60);
-    
-    // Animation basierend auf dem Status des Gegners
+
     setInterval(() => {
       if (this.isDead === false) {
         this.playAnimation(this.IMAGES_WALKING);
@@ -49,5 +64,4 @@ class Chicken extends MovableObject {
       }
     }, 200);
   }
-  
 }
